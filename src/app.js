e98bba1af5369c30e92ad445c1f7b8bcb9f12945
Jesús Salazar Andrade <salazarandradejesus@gmail.com>
2021-04-07 10:28:25 -0400

    subiendo los archivos nuevamente

--- src/app.js
+++ src/app.js
@@ -2,10 +2,35 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+  console.log("Hello from the console! ");
+};
+
+let generateExcuse = function() {
+  let who = ["Carlos", "Cristiano Ronaldo", "His max steel", "My grandpa"];
+  let action = ["ate", "smashed", "crushed", "broke"];
+  let what = ["my cantv modem", "my keys", "my car"];
+  let when = [
+    "before the javascript class",
+    "right on time",
+    "when I finished",
+    "during my breakfast",
+    "while I was meditating in my project"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
